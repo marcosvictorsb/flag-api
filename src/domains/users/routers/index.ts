99f85787eb846ee219cf3822312351cafebdf4b5
marcos <marcovictorsb@gmail.com>
2025-05-24 21:09:26 -0300

    chore: adicionando collection

--- src/domains/users/routers/index.ts
+++ src/domains/users/routers/index.ts
@@ -1,12 +1,17 @@
 import { Request, Response, Router } from 'express';
 import * as factories from '../factories';
+import { validateSchema } from '../../../middlewares/validate.schema';
+import { createUserSchema } from '../schemas/';
 
 const { createUserController } = factories;
 
 const router = Router();
 
-router.post('/', async (request, response) => {
-  await createUserController.createUser(request, response);
-});
+router.post(
+  '/',
+  validateSchema(createUserSchema),
+  (request: Request, response: Response) =>
+    createUserController.createUser(request, response)
+);
 
 export default router;
---
+++ src/domains/users/schemas/create.user.validator.ts
@@ -0,0 +1,13 @@
+// src/schemas/user.schema.ts
+import { z } from 'zod';
+
+export const createUserSchema = z.object({
+  body: z.object({
+    name: z
+      .string()
+      .min(3, 'Nome deve ter no mínimo 3 caracteres')
+      .max(80, 'Nome deve ter no máximo 50 caracteres'),
+    email: z.string().email('E-mail inválido'),
+    password_hash: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres')
+  })
+});
